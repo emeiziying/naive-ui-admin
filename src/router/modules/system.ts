510701ae8da9068764e042aff059df89709b84b7
dongzhangquan <dongzhangquan@boeet.com.cn>
2025-03-14 09:10:58 +0800

    ✨ feat(clear views and routers):

--- src/router/modules/system.ts
+++ src/router/modules/system.ts
@@ -1,7 +1,7 @@
-import { RouteRecordRaw } from 'vue-router';
 import { Layout } from '@/router/constant';
-import { OptionsSharp } from '@vicons/ionicons5';
 import { renderIcon } from '@/utils/index';
+import { OptionsSharp } from '@vicons/ionicons5';
+import { RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -12,7 +12,7 @@ const routes: Array<RouteRecordRaw> = [
     meta: {
       title: '系统设置',
       icon: renderIcon(OptionsSharp),
-      sort: 1,
+      sort: 6,
     },
     children: [
       {
